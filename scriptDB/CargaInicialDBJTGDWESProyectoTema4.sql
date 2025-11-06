2a62ce2acce6ce6bb39b2e12a80f74f282a84511
yatusabebeibe <jesus.temgal@educa.jcyl.es>
2025-11-06 19:35:10 +0100

    refactor: modificar scirpt cargaInicial para mas datos

--- scriptDB/CargaInicialDBJTGDWESProyectoTema4.sql
+++ scriptDB/CargaInicialDBJTGDWESProyectoTema4.sql
@@ -2,7 +2,7 @@ use DBJTGDWESProyectoTema4;
 
 insert into T02_Departamento (T02_CodDepartamento,T02_DescDepartamento,T02_FechaCreacionDepartamento,T02_VolumenDeNegocio,T02_FechaBajaDepartamento)
 values
-    ('TES','Desc test',now(),1235.5,null),
-    ('INF','Dept Informatica',now(),1235.5,null),
-    ('MUS','Dept Musica',now(),1235.5,null)
+    ('TES','Desc test', NOW() - INTERVAL 3 MONTH, 1235.5, NOW() - INTERVAL 43 DAY),
+    ('INF','Dept Informatica', NOW() - INTERVAL 2 WEEK, 1235.5 ,NULL),
+    ('MUS','Dept Musica', NOW(), 1235.5, NULL)
 ;
